78a0fa167d3ff0e23b736aaa1e2f7cc63935bbde
nwre87 <nwre87@gmail.com>
2016-07-30 12:56:19 -1000

    shorthand variable declaration

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
   "fmt"
-  "os"
 )
 
 func main() {
-  if len(os.Args) != 2 {
-    os.Exit(1)
-  }
-  fmt.Println("It's over", os.Args[1])
+  power := 9000
+  fmt.Printf("Its over %d\n", power)
 }
