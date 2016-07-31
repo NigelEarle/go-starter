e69bd22836db623ffb4a736f93c5914b7efc0b39
nwre87 <nwre87@gmail.com>
2016-07-30 17:44:54 -1000

    composition (structure inheritance)

--- main.go
+++ main.go
@@ -4,20 +4,28 @@ import (
   "fmt"
 )
 
-type Saiyan struct {
+type Person struct {
   Name string
-  Power int
-  Father *Saiyan
 }
 
-func main(){
-  gohan := &Saiyan{
-    Name: "Gohan",
-    Power: 1000,
-    Father: &Saiyan {
-      Name: "Goku",
-      Power: 9001,
-      Father: nil,
-    },
+// Introduce method on Person structure
+func (p *Person) Introduce() {
+  fmt.Printf("Hi, Im %s\n", p.Name)
+}
+
+// inherits Person structure through pointer
+type Saiyan struct {
+  *Person
+  Power int
+}
+
+func main() {
+  // instantiate Saiyan structure with Person address, name = Goku inherits to goku var
+  goku := &Saiyan {
+    Person: &Person{"Goku"},
+    Power: 9001,
   }
+  // call introduce method
+  fmt.Printf(goku.Name)
+  goku.Introduce()
 }
