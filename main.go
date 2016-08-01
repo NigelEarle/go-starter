3e04f9c8ab49e748d40f55b63e2fc58f106fb25a
nwre87 <nwre87@gmail.com>
2016-07-31 18:15:46 -1000

    slices intro; set values to array

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 )
 
 func main(){
-  // slices; creating array with unkown array indexes
-  scores := []int{301, 12, 87, 23}
+  // make creates a slice which represents a portion of an array
+  scores := make([]int,0,10)
+  // append values to slice, indexes 1-7
+  scores = scores[0:8]
+  scores[7] = 9033
   fmt.Println(scores)
+  // prints [0 0 0 0 0 0 0 9033]
 }
