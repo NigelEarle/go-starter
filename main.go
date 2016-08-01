8de7afea4de59be4b345ea323eacc0b955279ea4
nwre87 <nwre87@gmail.com>
2016-07-31 18:36:02 -1000

    usage of append() and cap() on growing array

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 )
 
 func main(){
-  // make creates a slice which represents a portion of an array
-  scores := make([]int,0,10)
-  // append values to slice, indexes 1-7
-  scores = scores[0:8]
-  scores[7] = 9033
-  fmt.Println(scores)
-  // prints [0 0 0 0 0 0 0 9033]
+  scores := make([]int,0 ,5)
+  c := cap(scores)
+  fmt.Println(c)
+
+  for i := 0; i < 25; i++ {
+    scores = append(scores, i)
+    fmt.Println(scores)
+    // if capactiy changes, the array grows to accomodate the data
+    if cap(scores) != c {
+      c = cap(scores)
+      fmt.Println(c)
+    }
+  }
 }
