17e4a3a07a502d304d0f9b569302beed12d72146
nwre87 <nwre87@gmail.com>
2016-08-01 16:31:04 -1000

    map inside of structure

--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
   "fmt"
-  "math/rand"
-  "sort"
 )
 
-func main(){
-  scores := make([]int, 100)
-  for i := 0; i < 100; i++ {
-    scores[i] = int(rand.Int31n(1000))
-  }
-  sort.Ints(scores)
-  fmt.Println("outside loop",scores)
+type Saiyan struct {
+  Name string
+  Friends map[string]*Saiyan
+}
 
-  worst := make([]int, 5)
-  copy(worst, scores[:5])
-  fmt.Println("worst",worst)
+func main(){
+  goku := &Saiyan{
+    Name: "Goku",
+    Friends: make(map[string]*Saiyan),
+  }
+  
+  goku.Friends["Krillin"] = ... // new structure
+  fmt.Println(goku)
 }
