e0f6ddfb5894be8c28a6f08296707138af3084b5
nwre87 <nwre87@gmail.com>
2016-08-02 22:20:05 -1000

    advanced channels

--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
   "fmt"
+  // "time"
+  "math/rand"
 )
 
-func main(){
-  c := make(chan int)
-  worker(c)
+type Worker struct{
+  id int
 }
 
-func worker(c chan int) {
-  // access to the channel made from main()
-  fmt.Println(c)
+func main() {
+  c := make(chan int)
+  for i := 0; i < 5; i++ {
+    worker := Worker{id: i}
+    fmt.Println("WORKER IN FOR LOOP", worker)
+    go worker.process(c)
+  }
+
+  for {
+    fmt.Println("FOR IN MAIN")
+    c <- rand.Int()
+    time.Sleep(time.Millisecond * 50)
+  }
+}
+
+func (w Worker) process(c chan int) {
+  fmt.Println("WORKER PROCESS", w.id)
+  for {
+    fmt.Println("WORKER FOR PROCESS")
+    data := <-c
+    fmt.Printf("worker %d got %d\n", w.id, data)
+  }
 }
 
