c6938adcccc9ff22606b28b794489da9c2c81fe4
nwre87 <nwre87@gmail.com>
2016-08-02 17:25:35 -1000

    basic channel

--- main.go
+++ main.go
@@ -2,24 +2,15 @@ package main
 
 import (
   "fmt"
-  "time"
-  "sync"
-)
-
-var (
-  // create lock var from sync.Mutex
-  lock sync.Mutex
 )
 
 func main(){
-  go func() {
-    fmt.Println("concurrent function")
-    // lock and unlock methods on lock var protects function
-    lock.Lock()
-    defer lock.Unlock()
-  }()
-  time.Sleep(time.Millisecond * 10)
-  lock.Lock()
+  c := make(chan int)
+  worker(c)
 }
 
+func worker(c chan int) {
+  // access to the channel made from main()
+  fmt.Println(c)
+}
 
