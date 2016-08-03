d6f455ac228ac5488362a1ac5b451dceef84099b
nwre87 <nwre87@gmail.com>
2016-08-02 15:15:46 -1000

    using sync.Mutex to lock and unlock

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
   "fmt"
   "time"
+  "sync"
 )
-// VERY BAD EXAMPLE READING AND WRITING TO A VARIABLE(var counter) CONCURRENTLY
-var counter = 0
+
+var (
+  counter = 0
+  lock sync.Mutex // default of sync.Mutex is unlocked
+)
 
 func main(){
   for i := 0; i < 20; i++ {
@@ -15,27 +19,8 @@ func main(){
 }
 
 func incr() {
+  lock.Lock() // lock var counter
+  defer lock.Unlock() 
   counter++
   fmt.Println(counter)
 }
-
-// Output will print ...
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 10
-// 11
-// 12
-// 13
-// 14
-// 15
-// 16
-// 17
-// 18
-// 19
-// 9
